163b00e50161348f05bff656f5345f909cac39fe
jordzhang <jordzhang@gmail.com>
2015-03-13 02:02:59 +0800

    add after_save callback

--- lib/background_replacement.rb
+++ lib/background_replacement.rb
@@ -7,17 +7,32 @@ module BackgroundReplacement
     def background_replacement(video, background, options={})
 
       # TODO after_save ,invoke the "replace_background" method when the video and background are changed.
+      after_save BackgroundReplacement.ReplacementCallBacks.new(video, background, options), if: Proc.new{|m| m.send("#{video}_changed?") || m.send("#{background}_changed?")}
+      # mod = Module.new
+      # include mod
+      # mod.class_eval <<-EOF
+      #   def replace_background
+      #     BackgroundReplacement.replace_background(video, background, options)
+      #   end
+      # EOF
     end
   end
 
-  private
-  def replace_background(video, background, options={})
-    if video.present? && background.present?
-      replacement_version = options[:replacement_version] || :replacement
-      replacement_path = video.try(replacement_version).try(:current_path)
+
+  class ReplacementCallBacks
+
+    def intialize(video, background, options={})
+      @video = video
+      @background = background
+      @options = options
+    end
+    def after_save(m)
+      if @video.present? && @background.present?
+      replacement_version = @options[:replacement_version] || :replacement
+      replacement_path = @video.try(replacement_version).try(:current_path)
 
       if replacement_path.present?
-        current_path = video.current_path
+        current_path = @video.current_path
         # split audio to mp3
         audio_path = File.join(File.dirname(current_path), "tmpfile.mp3")
         file = ::FFMPEG::Movie.new(current_path)
@@ -35,7 +50,6 @@ module BackgroundReplacement
 
         File.rename result_path, replacement_path
       end
-
     end
   end
 end
