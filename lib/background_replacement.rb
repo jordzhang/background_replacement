7fdde251098146496e5efc9eef64f977d196d29b
jordzhang <jordzhang@gmail.com>
2015-03-13 13:42:11 +0800

    add ffmpeg options

--- lib/background_replacement.rb
+++ lib/background_replacement.rb
@@ -4,26 +4,26 @@ module BackgroundReplacement
   end
 
   module ClassMethods
-    def background_replacement(video, background, options={})
-      after_save BackgroundReplacement::ReplacementCallBacks.new(video, background, options), if: Proc.new{|m| m.send("#{video}_changed?") || m.send("#{background}_changed?")}
+    def background_replacement(video, background, replacement_version, options={})
+      after_save BackgroundReplacement::ReplacementCallBacks.new(video, background, replacement_version, options), if: Proc.new{|m| m.send("#{video}_changed?") || m.send("#{background}_changed?")}
     end
   end
 
 
   class ReplacementCallBacks
 
-    def initialize(video, background, options={})
+    def initialize(video, background, replacement_version, options={})
       @video = video
       @background = background
       @options = options
+      @replacement_version = replacement_version
     end
     def after_save(m)
 
       video = m.send("#{@video}")
       background = m.send("#{@background}")
       if video.present? && background.present?
-        replacement_version = @options[:replacement_version] || :replacement
-        replacement_path = video.try(replacement_version).try(:current_path)
+        replacement_path = video.try(@replacement_version).try(:current_path)
 
         if replacement_path.present?
           current_path = video.current_path
@@ -42,7 +42,8 @@ module BackgroundReplacement
           result_path = File.join(File.dirname(current_path), "tmpfile.mp4")
 
           movie = ::FFMPEG::Movie.new(ouput_path)
-          movie.transcode(result_path, "-i #{audio_path} ")
+
+          movie.transcode(result_path, (@options || {}).merge({watermark: audio_path}))
 
           File.rename result_path, replacement_path
         end
